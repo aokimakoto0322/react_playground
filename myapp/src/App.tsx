0e5061cd445ed885164cdf4461857a102196b2f9
aokimakoto0322 <aokimakoto0322@gmail.com>
2024-05-31 17:21:53 +0900

    フロントの表示画面のみ実装

--- myapp/src/App.tsx
+++ myapp/src/App.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [cart, setCart] = useState([])
+
+  // 画面表示時
+  useEffect(() => {
+
+  })
+
+  const products = ["りんご", "ぶどう", "バナナ", "キウイ"]
+
   return (
     <div className="App">
       <header className="App-header">
@@ -19,6 +29,17 @@ function App() {
           Learn React
         </a>
       </header>
+      <div className='App-body'>
+        <h1 style={{"textDecoration": "underline"}}>買い物画面</h1>
+        <h2>かごの中身: {cart.join("、")}</h2>
+        <h2>商品一覧</h2>
+        { products.map((product) => (
+          <div>
+            <h3>・{ product }</h3>
+            <button>追加</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
